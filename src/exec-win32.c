e43bd857b9d0d60aeb2b8f0a12856bbe03c7796a
Parker Snell <parkersnell@gmail.com>
2024-01-13 18:09:46 -0700

    fix windows exec

--- src/exec-win32.c
+++ src/exec-win32.c
@@ -2,14 +2,24 @@
 #include <stdio.h>
 
 int yy_exec(char *const *argv) {
-  size_t argsize = 0;
-  for (int i = 1; argv[i]; ++i) {
+  size_t argsize = 2;
+  for (int i = 0; argv[i]; ++i) {
     argsize += strlen(argv[i]) + 1;
   }
   char *args = malloc(argsize);
-  size_t offset = 0;
+
+  size_t offset = 1;
+  args[0] = '"';
+  size_t length = strlen(argv[0]);
+  memcpy(args + 1, argv[0], length);
+  offset += length;
+  args[offset] = '"';
+  ++offset;
+  args[offset] = ' ';
+  ++offset;
+
   for (int i = 1; argv[i]; ++i) {
-    size_t length = strlen(argv[i]);
+    length = strlen(argv[i]);
     memcpy(args + offset, argv[i], length);
     offset += length;
     args[offset] = ' ';
@@ -17,12 +27,14 @@ int yy_exec(char *const *argv) {
   }
   args[offset - 1] = 0;
 
-  printf("args: %s \"%s\"\n", argv[0], args);
+  printf("args: %s\n", args);
 
   STARTUPINFO si;
+  memset(&si, 0, sizeof(STARTUPINFO));
+  si.cb = sizeof(STARTUPINFO);
   PROCESS_INFORMATION pi;
   if (CreateProcessA(
-      argv[0], args, NULL, NULL, TRUE, NORMAL_PRIORITY_CLASS, NULL, NULL,
+      NULL, args, NULL, NULL, TRUE, NORMAL_PRIORITY_CLASS, NULL, NULL,
       &si, &pi) == 0) {
     pi.dwProcessId = 0;
   }
